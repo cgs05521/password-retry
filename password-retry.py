1c4e84905e60ee6bbbddd94b6cc81c01e55247ef
bo cheng <cgs05521@gmail.com>
2019-12-21 14:45:14 +0800

    new retry

--- password-retry.py
+++ password-retry.py
@@ -3,15 +3,14 @@ test = int(test)
 password = 'a123456'
 
 while test > 0 :
+	test = test-1
 	name = input('請輸入密碼： ')
 	if name == password:
 		print('登入成功')
+		break
 	else:
-		test = test-1
-		if test == 0:
-			break
-		print('還有',test,'機會')
-
-
-
-print('恭喜猜錯')
+		print('密碼錯誤')
+		if test > 0:
+			print('還有',test,'機會')
+		else:
+			print('沒機會嘗試了')
